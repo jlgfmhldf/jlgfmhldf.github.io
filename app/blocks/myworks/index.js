57cbfc3f0a8d4d1c0e7852346292a61170ac6809
Вика Земцева <jlgfmhldf@gmail.com>
2017-10-15 17:36:33 +0300

    Feature/blurred image (#1)
    * fix(images/): optimize images
    
    * feat(blurred-image): added blurred-image in start, added js, added in ui-kit
    
    * feat(blurred-image): add transition to blur 0
    
    * fix(start): bg styles
    
    * fix lint errors
    
    * fix(blurred-image): remove blur class
    
    * feat(templates): add func for jade imageToBase64
    
    * small fixes

--- app/blocks/myworks/index.js
+++ app/blocks/myworks/index.js
@@ -1,59 +1,61 @@
 import Shuffle from 'shufflejs'
 
-const container = document.querySelector('.js-container')
-const filterBtns = document.querySelectorAll('.js-filter-btn')
-const clearFiltersBtn = document.querySelector('.js-clear-filters')
+const container = document.querySelector('.js-container') // TODO rename
+const filterBtns = document.querySelectorAll('.js-filter-btn') // TODO rename
+const clearFiltersBtn = document.querySelector('.js-clear-filters')  // TODO rename
 
-const filterBtnActiveClass = 'chip_active'
+if (container && filterBtns) {
+	const filterBtnActiveClass = 'chip_active'
 
-const shuffleOptions = {
-	itemSelector: '.js-container-item'
-}
+	const shuffleOptions = {
+		itemSelector: '.js-container-item'
+	}
 
-const shuffle = new Shuffle(container, shuffleOptions)
+	const shuffle = new Shuffle(container, shuffleOptions)
 
-const filterByTag = name => shuffle.filter(name)
+	const filterByTag = name => shuffle.filter(name)
 
-const removeActiveClassFromFilterBtn = el => el.classList.remove(filterBtnActiveClass)
+	const removeActiveClassFromFilterBtn = el => el.classList.remove(filterBtnActiveClass)
 
-const clearFilters = () => shuffle.filter('')
+	const clearFilters = () => shuffle.filter('')
 
-const toggleActiveClass = activeTag => {
-	const { innerText } = activeTag
-	const filterFunc = item => item.innerText !== innerText
-	const btnsForClassRemoving = [...filterBtns].filter(filterFunc)
+	const toggleActiveClass = activeTag => {
+		const { innerText } = activeTag
+		const filterFunc = item => item.innerText !== innerText
+		const btnsForClassRemoving = [...filterBtns].filter(filterFunc)
 
-	activeTag.classList.toggle(filterBtnActiveClass)
+		activeTag.classList.toggle(filterBtnActiveClass)
 
-	btnsForClassRemoving.forEach(removeActiveClassFromFilterBtn)
-}
+		btnsForClassRemoving.forEach(removeActiveClassFromFilterBtn)
+	}
 
-const handleFilterBtnClick = event => {
-	const { target } = event
-	const { name } = target.dataset
+	const handleFilterBtnClick = event => {
+		const { target } = event
+		const { name } = target.dataset
 
-	event.preventDefault()
+		event.preventDefault()
 
 
 
-	if (target.classList.contains(filterBtnActiveClass)) {
+		if (target.classList.contains(filterBtnActiveClass)) {
+			clearFilters()
+		}
+		else {
+			filterByTag(name)
+		}
+
+		toggleActiveClass(target)
+	}
+
+	const handleClearFilters = () => {
 		clearFilters()
-	}
-	else {
-		filterByTag(name)
+		filterBtns.forEach(removeActiveClassFromFilterBtn)
 	}
 
-	toggleActiveClass(target)
+	if (clearFiltersBtn) {
+		clearFiltersBtn.addEventListener('click', handleClearFilters)
+	}
+
+
+	filterBtns.forEach(item => item.addEventListener('click', handleFilterBtnClick))
 }
-
-const handleClearFilters = () => {
-	clearFilters()
-	filterBtns.forEach(removeActiveClassFromFilterBtn)
-}
-
-if (clearFiltersBtn) {
-	clearFiltersBtn.addEventListener('click', handleClearFilters)
-}
-
-
-filterBtns.forEach(item => item.addEventListener('click', handleFilterBtnClick))
